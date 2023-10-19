a088a9d1a6b61f8021e35ff919cda07d72600ad9
fojanb <fozhanmail@gmail.com>
2023-10-18 21:35:32 -0700

    Done

--- day-1-background-color-switcher/src/components/Colors/Colors.jsx
+++ day-1-background-color-switcher/src/components/Colors/Colors.jsx
@@ -2,21 +2,24 @@ import Color from "../Color/Color";
 import { useState } from "react";
 import "./Colors.css";
 const Colors = () => {
-  const [bgColor, setBgColor] = useState("#fff");
+  const [bgColor, setBgColor] = useState("#373f51");
   const colors = [
-    { color: "red", id: 20 },
-    { color: "blue", id: 21 },
-    { color: "green", id: 22 },
-    { color: "pink", id: 23 },
-    { color: "orange", id: 24 },
+    { color: "#ffffff", id: 20 },
+    { color: "#e0b1cb", id: 21 },
+    { color: "#246a73", id: 22 },
+    { color: "#691e06", id: 23 },
+    { color: "#b69121", id: 24 },
   ];
+  const setBackgroundColor = () => {
+    document.body.style.backgroundColor = bgColor;
+  }
   const clickHandler = (e) => {
     setBgColor(e.target.style.backgroundColor);
-    document.body.style.backgroundColor = bgColor;
   };
   const renderedColors = colors.map((color, index) => (
     <Color color={color.color} key={index} click={(e) => clickHandler(e)} />
   ));
+  setBackgroundColor();
   return (
     <div>
       <div className="colors">{renderedColors}</div>
