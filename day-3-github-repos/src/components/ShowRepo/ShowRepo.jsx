d210fe08394826ab95ef6436fba6ec3a2a07493f
Fojan Babaali <fojan.Babaali@carmanah.com>
2023-10-30 15:35:57 -0700

    Done

--- day-3-github-repos/src/components/ShowRepo/ShowRepo.jsx
+++ day-3-github-repos/src/components/ShowRepo/ShowRepo.jsx
@@ -1,11 +1,15 @@
-const ShowRepo = ({ forks, name, html_url }) => {
+const ShowRepo = ({ forks, name, html_url, stars }) => {
+  const style = { textAlign: "center" }
   return (
     <>
       <div className="repo">
-        <a href={html_url} target="_blank">
-          <div>{name}</div>
-        </a>
-        <div>{forks}</div>
+        <div>
+          <a href={html_url} target="_blank" rel="noopener noreferrer">
+            {name}
+          </a>
+        </div>
+        <div style={style}>{forks}</div>
+        <div style={style}>{stars}</div>
       </div>
     </>
   );
--- day-3-github-repos/src/components/ShowRepos/ShowRepos.jsx
+++ day-3-github-repos/src/components/ShowRepos/ShowRepos.jsx
@@ -1,10 +1,36 @@
 import "./ShowRepos.scss";
 import ShowRepo from "../ShowRepo/ShowRepo";
 const ShowRepos = ({ data }) => {
+  let styles = [
+    { textAlign: "center", color: "white" },
+    { textDecoration: "none", color: "white" },
+    { backgroundColor: "#99c1b9" },
+  ];
   let renderedData = data.map((repo) => {
-    const { id, name, forks,html_url } = repo;
-    return <ShowRepo key={id} name={name} forks={forks} html_url={html_url}/>;
+    const { id, name, forks, html_url, stargazers_count } = repo;
+    return (
+      <ShowRepo
+        key={id}
+        name={name}
+        forks={forks}
+        html_url={html_url}
+        stars={stargazers_count}
+      />
+    );
   });
-  return <div className="repos">{renderedData}</div>;
+  return (
+    <div className="repos">
+      <div className="repo" style={styles[2]}>
+        <div>
+          <a href="#" style={styles[1]}>
+            Repository Name
+          </a>
+        </div>
+        <div style={styles[0]}>Forks</div>
+        <div style={styles[0]}>Stars</div>
+      </div>
+      {renderedData}
+    </div>
+  );
 };
 export default ShowRepos;
